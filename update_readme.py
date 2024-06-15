4faffdb99fd211a00bc1b9cc6ec2bbd8e994ea85
Cary <45953972+FireJoker@users.noreply.github.com>
2024-06-15 14:50:08 -0600

    update new files

--- update_readme.py
+++ update_readme.py
@@ -1,13 +1,33 @@
-import re
 import json
-import datetime
+import os
+import requests # type: ignore
 
-def read_json_file(file_path):
-    """Reads a JSON file and returns the data."""
-    with open(file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
+def read_json_file(directory):
+    """Reads all JSON file and returns a list of data."""
+    data = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'r', encoding='utf-8') as file:
+                data.extend(json.load(file))
     return data
 
+def fetch_github_release_info(repo_url):
+    """Fetches the latest release info from a GitHub repository."""
+    # Extract owner and repo from the URL
+    parts = repo_url.rstrip('/').split('/')
+    owner = parts[-2]
+    repo = parts[-1]
+    
+    api_url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
+    response = requests.get(api_url)
+    response.raise_for_status()  # Raise an error for bad status codes
+    release_info = response.json()
+    
+    return {
+        "tag_name": release_info["tag_name"],
+        "published_at": release_info["published_at"]
+    }
 
 def generate_markdown_table(data):
     """Generates a Markdown table from a list of dictionaries."""
@@ -37,11 +57,11 @@ def update_readme(readme_path, table):
 
 
 if __name__ == "__main__":
-    json_file_path = 'data.json'  # Path to the JSON file
+    json_directory = 'src'  # Path to the JSON file
     readme_path = 'README.md'  # Path to the README.md file
     
     # Read and parse JSON file
-    data = read_json_file(json_file_path)
+    data = read_json_file(json_directory)
     
     # Generate Markdown table
     table = generate_markdown_table(data)
